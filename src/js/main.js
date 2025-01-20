1382c41b630f7e1bd7c30e5bdc074d2780ab914a
febe <febe.verelst@student.howest.be>
2025-01-20 14:54:01 +0100

    Flipping Card try

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
+const cardContainer = document.querySelector('.card__container');
 
+cardContainer.addEventListener('click', () => {
+    cardContainer.querySelector('.card').classList.toggle('flipped');
+});
