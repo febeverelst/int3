1745c64b3a491972e8e06a9f319fee1cf10cbc2b
febe <febe.verelst@student.howest.be>
2025-01-22 15:33:41 +0100

    Flipcard fix + campaign carousel (unfinished css)

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,27 @@
 const flipcardContainers = document.querySelectorAll('.flipcard__container');
+const carouselItems = document.querySelectorAll('.carousel__item');
+const buttons = document.querySelectorAll('.pagination-btn');
+
+const flipCard = (container, event) => {
+    if (!event.target.closest('.flipcard__button')) {
+        container.querySelector('.flipcard').classList.toggle('flipped');
+    }
+};
 
 flipcardContainers.forEach(container => {
-    container.addEventListener('click', () => {
-        container.querySelector('.flipcard').classList.toggle('flipped');
+    container.addEventListener('click', (event) => flipCard(container, event));
+});
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        carouselItems.forEach(item => item.style.transform = `translateX(-${index * 100}%)`);
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
     });
 });
 
+const init = () =>{
+    buttons[0].classList.add('active');
+}
+
+init();
